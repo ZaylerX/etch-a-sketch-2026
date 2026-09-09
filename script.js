764140b63ca0ae21fadaee5f026a649a7bd46077
ZaylerX <ZaylerX@live.it>
2026-09-09 15:46:09 +0200

    Created button and bugs
    Created the button to restart. It asks for input,
    accepts the user input but ignores the creation
    of the new grid. Needs to be fixed.
    
    Moreover, the number of squares for the grid is now
    fixed, it should be dynamic.

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const container = document.querySelector("#container");
 
-for (i = 0; i < 16; i++) {
-        for (j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        container.appendChild(square);
-
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            container.appendChild(square);
+        }
     }
 }
+
+let input = 16;
+
+createGrid(input);
 
 const grid = document.querySelectorAll(".square")
 
 grid.forEach(element => 
-    element.addEventListener("mouseover", (event) => {
+    element.addEventListener("mouseover", () => {
         element.style.backgroundColor = "blue"
     }))
+
+const restart = document.querySelector("#restart");
+
+restart.addEventListener("click", () => {
+    input = prompt("How many squares per side do you want? ")
+    grid.remove();
+    createGrid(input);
+})
+
